8004fe5955d960d41629de7f93c43fc853cd3a7c
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-07-16 08:59:59 +0000

    Jul 16, 2025, 8:59 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -17,6 +17,7 @@ function draw() {
   stroke("indigo");
   strokeWeight(15);
   triangle(x, 70, 150, 70, 80, 25);
+  x = x + 20;
   // Challenge 2: A face using circle() function
 
   // Challenge 3: A house using rect() function
