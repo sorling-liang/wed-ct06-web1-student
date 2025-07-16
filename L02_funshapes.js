ecc5a66b741497804807350671bd2919faea65f6
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-07-16 08:06:33 +0000

    Jul 16, 2025, 8:06 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -2,7 +2,8 @@
 Task 1: Add the setup() function
 */
 function setup() {
-  createC
+  //           w     h
+  createCanvas(600, 400);
 }
 function draw() {
   // Challenge 1: An eye using ellipse() function
