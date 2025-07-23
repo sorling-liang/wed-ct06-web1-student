2c8f0051633ff29ea369272694f72f7b3110eab5
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-07-23 07:55:35 +0000

    Jul 23, 2025, 7:55 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -1,7 +1,7 @@
 function setup() {
     createCanvas(600, 400);
     background(220);
-    noLoop(); // Stops continuous drawing
+    // noLoop(); // Stops continuous drawing
   }
   
 function draw() {
