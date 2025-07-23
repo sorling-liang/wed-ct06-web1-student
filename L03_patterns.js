bae6639bf992384b3737357689b4b8c461a676c2
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-07-23 08:21:40 +0000

    Jul 23, 2025, 8:21 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -13,7 +13,7 @@ function draw() {
 
   // Recap 1: Repeating Circles
   for (let i=0; i<6; i++) {
-    fill(i*50,0,2);
+    fill(i*50, 0, 225);
     circle(posx, 200, diameter);
     posx = posx + diameter + 10; // calculate the new position of x
   }
