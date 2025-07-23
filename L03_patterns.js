03f60c24cd5f8ca828a4fbf9fe93154769bdb432
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-07-23 08:38:28 +0000

    Jul 23, 2025, 8:38 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -9,17 +9,24 @@ function setup() {
 // forever loop
 function draw() {
 
-
-
-
-
   // Task 1: Colour Gradient
+  // let diameter = 80;
+  // let posx = 75;
+
+  // // Recap 1: Repeating Circles
+  // for (let i=0; i<6; i++) {
+  //   fill(0, i*50, 200); // red,green,blue
+  //   circle(posx, 200, diameter);
+  //   posx = posx + diameter + 10; // calculate the new position of x
+  // }
 
   // Task 2: Colour Loop
   fill(colorValue, 0, 0);
   circle(250,250, 200);
   // colorValue = colorValue +1;
   colorValue++; // shorthand
+
+  
   // Task 3: Row of Circles
 
   // Task 4: Grid of Circles
