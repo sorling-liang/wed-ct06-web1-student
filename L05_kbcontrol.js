7a067c984ce1dbfb2c7d287eabf9582e3f951cb3
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-08-06 07:58:21 +0000

    Aug 6, 2025, 7:58 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -3,7 +3,7 @@
 // run once similar to "when green flag clicked"
 function setup() {
     // get a blank drawing canvas
-    createCanvas(600, 4);
+    createCanvas(600, 400);
 
     // set a gray background
     background(200);
