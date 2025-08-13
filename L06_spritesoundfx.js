3dc25bda8ab60f5508ce5391d07ae8a4fa32790d
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-08-13 08:56:53 +0000

    Aug 13, 2025, 8:56 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -1,6 +1,10 @@
 // write your codes here
 let soundEffect, bgMusic, staticImage;
 
+function preload() {
+    
+}
+
 // run once similar to "when green flag clicked"
 function setup() {
     createCanvas(600, 400);
