e271a84b7d1c560c1d2151472d5208bcfc35fa85
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-08-13 07:42:48 +0000

    Aug 13, 2025, 7:42 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -15,21 +15,6 @@ function draw() {
     background(200); // clear the drawing paper
     //noStroke();
 
-    // draw a shape
-    // rect(100, 100, rectSize, rectSize);
-    // if (key === 'c') {
-    //     // circle diameter: 200
-    //     circle(width/2, height/2, 200);
-    // }
-    // if (key === 's') {
-    //     // square
-    //     rect(50, 50, 100, 100);
-    // }    
-    // if (key === 't') {
-    //     // triangle( x1,y1,  x2,y2,  x3,y3 )
-    //     triangle(0,200,  100,0,  200,200);
-    // }
-
     textSize(48); // font size
         // text     x  y
     text(key,     250, 70);
