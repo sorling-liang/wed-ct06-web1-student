a985afb79e0fc4e53cd034dddfbc05faef35536b
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-08-20 08:56:05 +0000

    Aug 20, 2025, 8:56 AM

--- midsem-revision.js
+++ midsem-revision.js
@@ -10,6 +10,8 @@ function setup() {
 // forever block
 function draw() {
     let xpos = 50;
+    let colorval = 0;
+    
     for (let count=0; count<5; count++ ) {
         rect(xpos,50,50,50);
         xpos = xpos+60;
