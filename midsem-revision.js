242809e910256e1d6a00831aed6d66997c87251b
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-08-20 07:23:31 +0000

    Aug 20, 2025, 7:23 AM

---
+++ midsem-revision.js
@@ -0,0 +1,12 @@
+// write your codes here
+
+// run once similar to "when green flag clicked"
+function setup() {
+    createCanvas(600, 400);
+    background("skyblue");
+}
+
+// forever block
+function draw() {
+
+}
