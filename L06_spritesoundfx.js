3a2256875797c047e0576d2ad49096186821527b
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-08-27 08:54:27 +0000

    Aug 27, 2025, 8:54 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -33,7 +33,7 @@ function draw() {
     // load picture onto the canvas
     image(staticImage, xpos, ypos, 110, 133);
 
-    image(staticImage, 250, 250, 110, 133);
+    image(secondguy, 250, 250, 110, 133);
 
     // // empty
     // fill("white");
