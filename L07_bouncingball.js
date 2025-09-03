62f483183c39187c79bdbbf756302242485374a1
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-09-03 07:47:47 +0000

    Sep 3, 2025, 7:47 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -3,7 +3,7 @@
 // run once similar to "when green flag clicked"
 function setup() {
     createCanvas(600, 400);
-    background("skyblue");
+    background("skyblue"); // 220
 }
 
 // forever block
