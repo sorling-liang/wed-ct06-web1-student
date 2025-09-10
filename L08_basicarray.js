fe3343f7b8253b691da26154bc0a1b694be3afed
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-09-10 08:03:17 +0000

    Sep 10, 2025, 8:03 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -10,16 +10,21 @@ function setup() {
     fill("yellow");
 }
 
-// forever block
 function draw() {
-    background(220);
-    noStroke();
-    rect( xpos, ypos, 50, 50 );
 
-    xpos = xpos + velocityX;
-
-    if ( xpos < 0 || xpos > width-50 ) {
-        fill( random(255), random(255), random(255) );
-        velocityX = velocityX * -1;
-    }
 }
+
+
+// // forever block
+// function draw() {
+//     background(220);
+//     noStroke();
+//     rect( xpos, ypos, 50, 50 );
+
+//     xpos = xpos + velocityX;
+
+//     if ( xpos < 0 || xpos > width-50 ) {
+//         fill( random(255), random(255), random(255) );
+//         velocityX = velocityX * -1;
+//     }
+// }
