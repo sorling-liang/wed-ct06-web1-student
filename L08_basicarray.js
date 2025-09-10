917250ec1719545bfa8cb61f340b8f9304db014e
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-09-10 08:30:18 +0000

    Sep 10, 2025, 8:30 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -1,3 +1,47 @@
+let velocityX = 2;
+let xpos = 200;
+let ypos =200;
+
+
+
+
+function setup() {
+    createCanvas(600,600);
+    background("black");
+    fill("cyan")
+    textSize(32);
+    let favFoods =  ["Gyudon", "Udon", "Sushi", "Pasta"];
+    // text("My name is Geng Woon", 50,50)
+    // text("I am 10 this year", 50, 100)
+    // text("My favourite activity is swimming", 50, 150)
+    console.log("I love" + favFoods.length + ".xoa")
+    // for ( let index=0; index < favFoods.length; index++) {
+    let yValue = 80;
+    for (let index =0; index < favFoods.length; index++) {
+        text( "I love " + favFoods[index] + ".", 80, yValue)
+        yvalue = yvalue + 55
+    }
+    }
+
+
+function draw() {
+// background(220); 
+// noStroke();
+// rect(xpos,ypos,50,50)
+
+// xpos = xpos + velocityX;
+
+// if ( xpos < 0 || xpos > width) {
+//     fill( random(255), random(255), random(255) );
+
+//     velocityX = velocityX 
+// } 
+
+
+
+}
+
+/*
 // write your codes here
 // let xpos = 200;
 // let ypos = 180;
@@ -44,3 +88,4 @@ function draw() {
 //         velocityX = velocityX * -1;
 //     }
 // }
+*/
