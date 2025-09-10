b398de57dcc3357fbeb95f75d827ddf95ccf4359
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-09-10 08:25:41 +0000

    Sep 10, 2025, 8:25 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -20,7 +20,7 @@ function setup() {
     let yValue = 80;
     for ( let index=0; index < favFoods.length; index++ ) {
         console.log( "I love " + favFoods[index] + "!!!" );
-        text( favFoods[index], 80, yValue );
+        text( "I love "  + favFoods[index], 80, yValue );
         yValue = yValue + 55;
         
     }
