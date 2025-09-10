fc83a189ad0e4f8932df95fc33d84b38b57f8abf
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-09-10 08:59:39 +0000

    Sep 10, 2025, 8:59 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -4,7 +4,7 @@ let ypos;
 
 function preload() {
     soundsFX = loadSound("assets/star_wars_theme_8_bit.mp3");
-    soundsFX.loop(); // continuous play
+    //soundsFX.loop(); // continuous play
 }
 
 // run once similar to "when green flag clicked"
