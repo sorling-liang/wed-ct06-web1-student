3d569ab9cc83d87684a4c93ee386c3e05092032d
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-09-17 07:37:27 +0000

    Sep 17, 2025, 7:37 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -25,7 +25,7 @@ function draw() {
 
     textAlign(CENTER, CENTER);
     text( "the beginning of the star wars story...", 0,0 );
-    text( "at episode 4 in the year 1995", 0, 40 );
+    text( "at episode 4 in the year 1995", 0, 50 );
 
     ypos = ypos - 1;
 
