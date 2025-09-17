468e4888d6c0993843c57655b9baa834cfefbc42
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-09-17 07:56:13 +0000

    Sep 17, 2025, 7:56 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -33,7 +33,7 @@ function draw() {
     let story = [
         "the beginning of the star wars story...",
         "at episode 4 in the year 1995",
-        "the end",
+        "the end. I hope you enjoyed this story.",
     ];
 
     for (let index=0; index< story.length; index++) {
