f18b3205d5b85b530986eb456f6691d6b7878c63
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-09-17 07:23:05 +0000

    Sep 17, 2025, 7:23 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -11,8 +11,8 @@ function setup() {
     createCanvas(600, 400);
     background("black");
     fill("cyan");
-    textSize(18);
-    
+    textSize(48);
+
     // this line must be in setup(), not preload() otherwise js error
     //soundsFX.loop(); // continuous play
 
