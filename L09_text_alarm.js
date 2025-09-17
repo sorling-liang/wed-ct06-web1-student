28388d4cda152d57c2fbc19702ec25afb09fd674
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-09-17 08:50:09 +0000

    Sep 17, 2025, 8:50 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -1,29 +1,29 @@
 // write your codes here
-let xpos;
-let velocityX;
+// let xpos;
+// let velocityX;
 
-// run once similar to "when green flag clicked"
-function setup() {
-    createCanvas(600, 400);
-    background("skyblue");
-    textSize(48);
-    textAlign(CENTER, CENTER);
+// // run once similar to "when green flag clicked"
+// function setup() {
+//     createCanvas(600, 400);
+//     background("skyblue");
+//     textSize(48);
+//     textAlign(CENTER, CENTER);
 
-    xpos = width/2;
-    velocityX = 2;
-}
+//     xpos = width/2;
+//     velocityX = 2;
+// }
 
-// forever block
-function draw() {
-    background("skyblue");
+// // forever block
+// function draw() {
+//     background("skyblue");
 
-    translate( xpos, height/2 );
-    text("Bounce!", 0, 0);
+//     translate( xpos, height/2 );
+//     text("Bounce!", 0, 0);
 
-    xpos = xpos + velocityX;
+//     xpos = xpos + velocityX;
 
-    // combine 2 ifs, into a OR condition
-    if (xpos > width || xpos < 0) {
-        velocityX = velocityX * -1;
-    }
-}
+//     // combine 2 ifs, into a OR condition
+//     if (xpos > width || xpos < 0) {
+//         velocityX = velocityX * -1;
+//     }
+// }
