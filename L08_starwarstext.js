c26b992c5cd3b45280f21eae8ce2e3e3f6e20280
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-09-17 07:57:00 +0000

    Sep 17, 2025, 7:57 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -24,6 +24,7 @@ function draw() {
     background("black"); // erase
 
     translate( width/2, ypos );
+    scale( 1, 3 );
 
     textAlign(CENTER, CENTER);
     //text( "the beginning of the star wars story...", 0,0 );
