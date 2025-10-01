468d2b9009dbe96bd596581b4f4b4f391d0a52a6
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-10-01 08:17:36 +0000

    Oct 1, 2025, 8:17 AM

--- L10_name.js
+++ L10_name.js
@@ -19,6 +19,7 @@ function draw() {
     rect(100,100,400,155, 25);
     fill("black");
     textSize(32);
-    textAlign(CENTER,CENTER);
+    //          X      Y
+    textAlign(CENTER, CENTER);
     text("Your name", width/2, 150);
 }
