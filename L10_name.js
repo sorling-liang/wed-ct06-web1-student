d1c9a05b8e603ed94b65b65be4580f04531f3764
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-10-01 09:00:26 +0000

    Oct 1, 2025, 9:00 AM

--- L10_name.js
+++ L10_name.js
@@ -15,7 +15,7 @@ function setup() {
     colorPicker = createColorPicker(bgColor);
     colorPicker.position( width/2, height/2+50 );
 
-    userInput = createInput(username);
+    userInput = createInput(username,"text");
     userInput.position( width/2, height/2+100)
     userInput.input(updateLabel);
 
