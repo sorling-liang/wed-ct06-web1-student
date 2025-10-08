e674799ff57ece378a5c6d4eed1c988545bbc99a
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-10-08 07:58:20 +0000

    Oct 8, 2025, 7:58 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -11,7 +11,7 @@ function setup() {
     // create a text input
     inputSomeText = createInput(story, "text");
     inputSomeText.position(50,50);
-    inputSomeText.size(500, 100);
+    inputSomeText.size(350);
 
     // i will teach create a button
 }
