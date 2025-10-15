8061db5fc52743ae21cee7e0be551deb9248c606
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-10-15 08:55:18 +0000

    Oct 15, 2025, 8:55 AM

--- revision.js
+++ revision.js
@@ -1,5 +1,6 @@
 let xpos = 60;
 let velocity = 15;
+let 
 
 function setup() {
     createCanvas(600,400);
@@ -7,14 +8,14 @@ function setup() {
 }
 
 function draw() {
-    background("indigo");
-    circle(xpos,50,50);
-    xpos = xpos + velocity;
+    // background("indigo");
+    // circle(xpos,50,50);
+    // xpos = xpos + velocity;
 
-    if (xpos > width) {
-        velocity = velocity * -1;
-    }
-    if (xpos < 0) {
-        velocity = velocity * -1;
-    }
+    // if (xpos > width) {
+    //     velocity = velocity * -1;
+    // }
+    // if (xpos < 0) {
+    //     velocity = velocity * -1;
+    // }
 }
