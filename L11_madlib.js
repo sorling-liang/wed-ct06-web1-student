492c02994fbfeb248a41918c2b7b2c6425024da3
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-10-15 08:27:17 +0000

    Oct 15, 2025, 8:27 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -87,6 +87,7 @@ function draw() {
     fill("white");
     noStroke();
     rect(50,300,500,200,25);
+    textSize()
     textAlign(CENTER, CENTER);
-    text(story,width/2, he)
+    text(story,width/2, height/2);
 }
