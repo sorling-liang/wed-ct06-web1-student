d56f062f5fd9b82fd39de0eff3ed18e1babeca03
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-10-29 07:58:10 +0000

    Oct 29, 2025, 7:58 AM

--- revision.js
+++ revision.js
@@ -1,4 +1,5 @@
 // question 1
+/*
 let posX, xSpeed;
 
 function setup() {
@@ -15,5 +16,15 @@ function draw() {
     posX = posX + xSpeed;
     
 }
-
+*/
 // question 2
+let pokemons = ["Pikachu","Raichu","Bulbasaur","Mewtwo","Charizard"];
+let yPos;
+
+function setup(){
+    createCanvas(200, 300);
+    fill("pink");
+    // Task: Align the text to the center using textAlign
+    // Task: set font size to 20 using textSize
+    yPos = 100;
+}
