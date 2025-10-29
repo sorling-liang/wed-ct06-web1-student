dbd3985b672bfed3b88215efeea326b6ccd41470
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-10-29 07:44:20 +0000

    Oct 29, 2025, 7:44 AM

--- revision.js
+++ revision.js
@@ -1,13 +1,14 @@
 // question 1
 let posX, xSpeed;
 
-function setup(){
+function setup() {
     createCanvas(600, 400);
     background(200);
     posX = width/ 2;
     xSpeed = 5;
     fill('limegreen');
     circle(width/2,height/2,50);
+    posX = posX + 50;
 }
 
 // repeat 60 times per one second
