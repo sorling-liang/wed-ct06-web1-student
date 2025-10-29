919be4d8786e076408753d45a7c219f11dcf01f8
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-10-29 07:50:47 +0000

    Oct 29, 2025, 7:50 AM

--- revision.js
+++ revision.js
@@ -12,7 +12,7 @@ function setup() {
 
 // repeat 60 times per one second
 function draw() {
-    background("lightblue");
+    background("'lightblue'");
     fill('limegreen');
     circle(posX, height/2, 50);
     posX = posX + xSpeed;
