90a98612e2b2dc0a09e2a94f928728699e733d7f
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-10-29 08:58:39 +0000

    Oct 29, 2025, 8:58 AM

--- L15_geometrydash.js
+++ L15_geometrydash.js
@@ -23,10 +23,10 @@ function draw() {
     fill("blue");
     rect(width/2, height/2, 50, 50);
 
-    drawSpike(840, 880, 600, 670);
+    drawSpike(840, 880, 600, 670); // 
 }
 
-function drawSpike(x1, x2, y1, orange2) {
+function drawSpike(x1, x2, y1, y2) {
     // probably green colour
     // triangle()
     fill("lime");
